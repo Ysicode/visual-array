37b673bdd648b2805a89b89d23652d45e6250804
Simon Weiss <simonweiss.ml@gmail.com>
2022-08-09 20:12:58 +0200

    added generate new array

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
+  @ViewChild('array') myarray: ElementRef | any;
+  arrayItems: number = 62;
+  myArr = Array(this.arrayItems);
 
+
+  arr = [];
+
+  arrayItem: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.generateArr();
+  }
+
+  generateArr() {
+      return Math.floor(Math.random() * 270);
+  }
+
+  visualize() {
+    return this.arr.map(element => element + 10)
   }
 
 }
