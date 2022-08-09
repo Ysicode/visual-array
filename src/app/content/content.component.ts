14e3b7c08d1566d637ee2ee8596f489e31f54f60
Simon Weiss <simonweiss.ml@gmail.com>
2022-08-09 23:03:04 +0200

    added animation

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -6,12 +6,11 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
-  @ViewChild('array') myarray: ElementRef | any;
-  arrayItems: number = 62;
-  myArr = Array(this.arrayItems);
-
-
-  arr = [];
+  @ViewChild("output") output: ElementRef;
+  myArr = Array(62);
+  item: number = 0;
+  arrayValue: number;
+  currentArray = [];
 
   arrayItem: any;
   constructor() { }
@@ -21,11 +20,68 @@ export class ContentComponent implements OnInit {
   }
 
   generateArr() {
-      return Math.floor(Math.random() * 270);
+    this.currentArray = [];
+    setTimeout(() => {
+      this.getArrValues();
+      this.refreshBackground();
+    }, 200);
+
+  }
+
+  getArrValues() {
+    let elements = Array.from(document.getElementsByClassName('array_element'));
+    for (let i = 0; i < elements.length; i++) {
+      let element: any = elements[i]
+      this.arrayValue = Math.floor(Math.random() * 270);
+      this.currentArray.push(this.arrayValue);
+      element.style.height = this.getheight();
+    }
+
+  }
+
+  getheight() {
+    return this.arrayValue + `px`
   }
 
   visualize() {
-    return this.arr.map(element => element + 10)
+    let elements = Array.from(document.getElementsByClassName('array_element'));
+    elements.forEach((element: any) => {
+      element.style.background = 'linear-gradient(#11998e, #38ef7d)'
+    })
+    this.item = 0;
+    this.animate(elements);
+  }
+
+  animate(elements: any) {
+    if (this.item == 62) {
+      this.showOutput();
+      return
+    }
+    let arrElement = elements[this.item];
+    arrElement.style.background = `red`;
+    this.item++;
+    setTimeout(() => {
+      this.animate(elements);
+    }, 30)
+  }
+
+  showOutput() {
+  
+    this.output.nativeElement.innerHTML = this.mapFunction();
+  }
+
+  mapFunction() {
+    return this.currentArray.map(element => element + ` hi`)
+  }
+
+  refreshBackground() {
+    this.item = 0;
+    let elements: any = Array.from(document.getElementsByClassName('array_element'));
+    elements.forEach((element: any) => {
+      element.style.background = 'linear-gradient(#11998e, #38ef7d)'
+    })
   }
 
 }
+
+
