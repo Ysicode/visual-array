2f09620e68a7aed6018298489c921ecf4b262815
Simon Weiss <simonweiss.ml@gmail.com>
2022-08-10 22:14:11 +0200

    added sort and find function

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -9,13 +9,20 @@ export class ContentComponent implements OnInit {
   @ViewChild("output") output: ElementRef;
   myArr = Array(62);
   item: number = 0;
+  sortedItem: number = 0;
   arrayValue: number;
   currentArray = [];
   arrayItem: any;
+  findElement: number = Math.floor(Math.random() * 60);
 
-  mapSelect: any = true;
-  everySelect: any = false;
-  someSelect: any = false;
+  methods = {
+    mapSelect: true,
+    everySelect: false,
+    someSelect: false,
+    filterSelect: false,
+    sortSelect: false,
+    findSelect: false,
+  }
 
   constructor() { }
 
@@ -23,51 +30,49 @@ export class ContentComponent implements OnInit {
     this.generateArr();
   }
 
+  getValue() {
+    let value = this.currentArray[this.findElement]
+    return value
+  }
+
   generateArr() {
     this.currentArray = [];
+    this.findElement = Math.floor(Math.random() * 60);
     setTimeout(() => {
       this.getArrValues();
       this.refreshBackground();
-    }, 200);
-
+    }, 50);
   }
 
   getArrValues() {
+    this.output.nativeElement.innerHTML = '';
     let elements = Array.from(document.getElementsByClassName('array_element'));
     for (let i = 0; i < elements.length; i++) {
       let element: any = elements[i]
       this.arrayValue = Math.floor(Math.random() * 270);
       this.currentArray.push(this.arrayValue);
-      element.style.height = this.getheight();
+      element.style.height = this.getheight(i);
     }
   }
 
-  getheight() {
-    return this.arrayValue + `px`
+  getheight(i: number) {
+    return this.currentArray[i] + `px`
   }
 
-  selectMethod(method: any) {
-    if (method == 'every') {
-      this.everySelect = true;
-      this.mapSelect = false;
-      this.someSelect = false;
-    }
-    if (method == 'map') {
-      this.everySelect = false;
-      this.mapSelect = true;
-      this.someSelect = false;
-    }
-    if (method == 'some') {
-      this.everySelect = false;
-      this.mapSelect = false;
-      this.someSelect = true;
+  selectMethod(index: string) {
+    this.generateArr();
+    for (let key in this.methods) {
+      this.methods[key] = false;
+      this.methods[index] = true;
     }
   }
 
   visualize() {
+    this.output.nativeElement.innerHTML = '';
     let elements = Array.from(document.getElementsByClassName('array_element'));
     elements.forEach((element: any) => {
-      element.style.background = 'linear-gradient(#11998e, #38ef7d)'
+      element.style.background = '';
+      element.style.background = 'linear-gradient(#9a9a04, #e0dd00);'
     })
     this.item = 0;
     this.animate(elements);
@@ -75,27 +80,75 @@ export class ContentComponent implements OnInit {
 
   animate(elements: any) {
     if (this.item == 62) {
+      if (this.methods.sortSelect) {
+        this.insertionSort();
+        this.animateSortedArray(elements);
+      }
       this.showOutput();
       return
     }
     let arrElement = elements[this.item];
     arrElement.style.background = `#cc91ca`;
+
+    if (this.methods.filterSelect) {
+      this.checkMatchingItems(arrElement);
+    }
+    if (this.methods.findSelect) {
+      const element = this.currentArray.find(element => element == this.currentArray[this.findElement])
+        if (this.currentArray[this.item] == element) {
+          arrElement.style.background = `red`;
+          this.showOutput();
+          return  
+      }
+    }
+
     this.item++;
     setTimeout(() => {
       this.animate(elements);
-    }, 30)
+    }, 10)
+  }
+
+  animateSortedArray(elements: any) {
+    if (this.sortedItem == 62) {
+      this.sortedItem = 0;
+      return
+    }
+    let arrElement = elements[this.sortedItem];
+    console.log(arrElement)
+    arrElement.style.background = `linear-gradient(#9a9a04, #e0dd00);`;
+    arrElement.style.height = this.getheight(this.sortedItem);
+    this.sortedItem++;
+    setTimeout(() => {
+      this.animateSortedArray(elements);
+    }, 10)
+  }
+
+  checkMatchingItems(arrElement: any) {
+    if (this.currentArray[this.item] > 200) {
+      arrElement.style.background = `red`;
+    }
   }
 
   showOutput() {
     this.output.nativeElement.innerHTML = '';
-    if (this.mapSelect) {
+    if (this.methods.mapSelect) {
       this.output.nativeElement.innerHTML = this.mapFunction();
     }
-    if (this.everySelect) {
+    if (this.methods.everySelect) {
       this.output.nativeElement.innerHTML = this.everyFunction();
     }
-    if (this.someSelect) {
+    if (this.methods.someSelect) {
       this.output.nativeElement.innerHTML = this.someFunction();
+    }
+    if (this.methods.filterSelect) {
+      this.output.nativeElement.innerHTML = this.filterFunction();
+    }
+    if (this.methods.sortSelect) {
+      this.sortFunction();
+      this.output.nativeElement.innerHTML = this.currentArray.join(', ');
+    }
+    if (this.methods.findSelect) {
+      this.output.nativeElement.innerHTML = this.findFunction();
     }
   }
 
@@ -108,7 +161,19 @@ export class ContentComponent implements OnInit {
   }
 
   someFunction() {
-    return this.currentArray.some(element => element > 20)
+    return this.currentArray.some(element => element > 100)
+  }
+
+  filterFunction() {
+    return this.currentArray.filter(element => element > 200)
+  }
+
+  sortFunction() {
+    return this.currentArray.sort((a, b) => a > b ? 1 : -1)
+  }
+
+  findFunction() {
+    return this.currentArray.find(element => element == this.currentArray[this.findElement])
   }
 
 
@@ -116,8 +181,23 @@ export class ContentComponent implements OnInit {
     this.item = 0;
     let elements: any = Array.from(document.getElementsByClassName('array_element'));
     elements.forEach((element: any) => {
-      element.style.background = 'linear-gradient(#11998e, #38ef7d)'
+      element.style.background = '';
+      element.style.background = 'linear-gradient(#9a9a04, #e0dd00);'
     })
+  }
+
+  async insertionSort() {
+    for (let i = 1; i < this.currentArray.length; i++) {
+      // Start comparing current element with every element before it
+      for (let j = i - 1; j > -1; j--) {
+
+        // Swap elements as required
+        if (this.currentArray[j + 1] < this.currentArray[j]) {
+          [this.currentArray[j + 1], this.currentArray[j]] = [this.currentArray[j], this.currentArray[j + 1]];
+        }
+      }
+    }
+    console.log(this.currentArray)
   }
 
 }
